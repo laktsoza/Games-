d39bad6e11e3a20babb3b9e6833dbeada26d5d2e
David Sozashvili <dato.soza.js@gmail.com>
2020-10-02 13:11:24 +0400

    ticTac

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle' 
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle'; 
 const WINNING_COMBINATIONS = [
     [0, 1, 2], 
     [3, 4, 5],
@@ -10,20 +10,20 @@ const WINNING_COMBINATIONS = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
 const cellElements = document.querySelectorAll ('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
+let circleTurn;
 
 startGame()
 
 restartButton.addEventListener('click', startGame)
 
 function startGame() {
-    circleTurn = false 
+    circleTurn = true 
     cellElements.forEach(cell => {
         cell.classList.remove(X_CLASS)
         cell.classList.remove(CIRCLE_CLASS)
